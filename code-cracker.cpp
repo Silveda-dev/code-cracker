a83354ccf15ea81713ee4495e4c792b8d00a2387
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-02-18 19:59:53 +1030

    Draft logic handling for entropy option

---
+++ code-cracker.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string encrypted, do_entropy;
+    float entropy;
+
+    cout << "Welcome to Code Cracker - helping you decrypt a range of ciphers." << endl;
+    cout << "Would you like to begin by calculating entropy and receiving suggestions on likely ciphers? (Y/N)  ";
+    cin >> do_entropy;
+    while (do_entropy != "Y" && do_entropy != "N") {
+        cout << "Please enter either Y or N. Re-enter here: ";
+        cin >> do_entropy;
+    }
+
+    if (do_entropy == "Y") {
+        //Call entropy calculation function
+    }
+
+    //Move to decrypting stage 
+}
