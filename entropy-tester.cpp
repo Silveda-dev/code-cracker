6bdeb75ca41ca1d2e4e4a73b7dfa1e1b787b2d42
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-02-20 17:55:27 +1030

    Test calc_frequencies function

--- entropy-tester.cpp
+++ entropy-tester.cpp
@@ -10,8 +10,18 @@ void test_letter_count(Entropy e) {
     return;
 }
 
+void test_letter_frequency(Entropy e) {
+    float* e_ptr = e.get_enc_letter_frequency();
+    for (int i = 0; i < 26; i++) {
+        std::cout << "Item " << i << ": " << *(e_ptr+i) << std::endl;
+    }
+
+    return;
+}
+
 int main() {
     Entropy e("cat");
     test_letter_count(e);
+    test_letter_frequency(e);
     return 0;
 }
