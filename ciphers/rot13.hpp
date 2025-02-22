b37d74c5b33d49a566ff6ffc64f20851d2a69033
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-02-22 21:52:27 +1030

    Account for uppercase letters

--- ciphers/rot13.hpp
+++ ciphers/rot13.hpp
@@ -2,12 +2,16 @@
 #define ROT13_HPP
 
 #include <string>
+#include <bits/stdc++.h>
 
 class Rot13 {
     private:
         std::string decrypted;
     public:
         Rot13(std::string encrypted) {
+            for (size_t x = 0; x < encrypted.size(); x++) {
+                encrypted[x] = tolower(encrypted[x]);
+            }
             decrypted = encrypted;
             for (size_t i = 0; i < encrypted.size(); i++) {
                 if (encrypted[i] >= 'n') {
