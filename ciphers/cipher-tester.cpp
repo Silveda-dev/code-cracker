c75d9e6da7e115a76aa0cabea4d8be034557db55
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-02-23 21:50:05 +1030

    Test Caesar cipher

--- ciphers/cipher-tester.cpp
+++ ciphers/cipher-tester.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 #include "rot13.hpp"
+#include "caesar.hpp"
 using namespace std;
 
 int main() {
     Rot13 r("aBcdeFghijklmNopqrstuvwxyz");
-    cout << r.get_decrypted() << endl;
-
+    cout << "ROT13 decryption: " << r.get_decrypted() << endl;
+    Caesar c("aBcdeFghijklmNopqrstuvwxyz", 11);
+    cout << "Caesar (11) decryption: " << c.get_decrypted() << endl;
     return 0;
 }
